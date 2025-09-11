fbff5536a63379f3c9555365b04bc6418aeb1617
David Popescu <davidpopescu511@yahoo.com>
2025-09-11 18:32:09 -0400

    Learning for Assignment 2

---
+++ Lectures/Week3/Wk3-Objectsfollowalong.ps1
@@ -0,0 +1,78 @@
+﻿#CMDLETs and objects
+# Get-Process
+
+# #0 Get a single process by name
+# $p = Get-Process -ProcessName "wireshark"
+# $p
+# $p | format-list *
+# # $p | fl *
+# Write-Host "Line 9"
+# $p.getType()
+# Write-Host "Line 11"
+# $p.getType().Name
+
+# Write-Host "----"
+# $p | Get-Member
+
+
+Write-Host "----"
+$processes = Get-Process
+$processes.GetType()
+
+$processes[10]
+Write-Host "----"
+$processes[10].GetType()
+
+$processes[10].CPU
+
+write-Host "----"
+$processes | Measure-Object
+
+
+$processes.Count
+
+
+$processes2 = $processes | Where-Object {$_.ID -lt 4000}
+
+$processes2.Count
+Write-Host "----"
+
+Get-ChildItem
+
+$files = Get-ChildItem
+Write-Host "----"
+Select-Object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# $p.GetType().Name
+# $p | Get-Member
+
+# #1. What type of Object is $processes
+
+# #2. What type of Object is our 11th element in our Array
+
+
+# #3. Ways to count the number of items in the array
+
+
+# # Use of fl (Format-List) to display all properties and current values
+
+
+# #Get-ChildItem
+
+
+# #Select-Object has a great way to pull in specific Object Properties
+
